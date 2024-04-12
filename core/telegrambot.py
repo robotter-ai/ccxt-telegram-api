eff3675512f36940b170011eaea7d9712edd24ed
hudson-oliveira-funttastic <hudson.oliveira@funttastic.com>
2024-04-12 19:06:08 -0300

    Adding new version for the telegrambot with the channelid.

--- core/telegrambot.py
+++ core/telegrambot.py
@@ -16,16 +16,15 @@ CONFIRM, CANCEL, END_CONVERSATION = range(12, 15)
 
 
 class TelegramBot:
-    def __init__(self, token: str, allowed_user_id, trade_executor: TradeExecutor):
+    def __init__(self, token: str, channel_id: str, trade_executor: TradeExecutor):
         self.application = Application.builder().token(token).build()
         self.trade_executor = trade_executor
         self.exchange = self.trade_executor.exchange
-        # self.private_filter = filters.User(user_id=int(allowed_user_id))
+        self.channel_id = channel_id
         self._prepare()
 
     def _prepare(self):
         self.application.add_handler(CommandHandler('start', self.show_help))
-        # self.application.add_handler(CommandHandler('start', self.show_help, filters=self.private_filter))
         self.application.add_handler(self.build_conversation_handler())
         self.application.add_error_handler(self.handle_error)
 
@@ -60,13 +59,11 @@ class TelegramBot:
     def build_conversation_handler(self) -> ConversationHandler:
         return ConversationHandler(
             entry_points=[CommandHandler('trade', self.show_options)],
-            # entry_points=[CommandHandler('trade', self.show_options, filters=self.private_filter)],
             states={
                 TRADE_SELECT: [CallbackQueryHandler(self.process_trade_selection)],
                 # Add other states and their handlers here
             },
             fallbacks=[CommandHandler('start', self.show_help)],
-            # fallbacks=[CommandHandler('start', self.show_help, filters=self.private_filter)],
         )
 
     def handle_error(self, update: object, context: CallbackContext) -> None:
