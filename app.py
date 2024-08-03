97bedd6579af2a5e10131f0478d550c6d092665b
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-08-03 17:43:48 -0300

    Improving isSignedIn endpoint.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 import signal
 import uvicorn
 from dotmap import DotMap
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, HTTPException
 from pathlib import Path
 from starlette.requests import Request
 from starlette.responses import JSONResponse
@@ -105,36 +105,47 @@ async def auth_refresh(request: Request, response: Response):
 @app.get("/auth/isSignedIn")
 @app.post("/auth/isSignedIn")
 async def is_signed_in(request: Request, response: Response):
-	await validate(request)
-
-	parameters = await extract_all_parameters(request)
-
-	token = extract_jwt_token(parameters)
-
-	user = get_user(token)
-
 	response = APIResponse()
 
-	if user or not validate_request_token(token):
-		response.title = "User is Signed In"
-		response.message = "User has already signed in."
-		response.result = True
+	try:
+		await validate(request)
 
-		json_response = JSONResponse(
-			status_code=APIResponseStatus.SUCCESS.http_code,
-			content=response.toDict()
-		)
-	else:
-		response.title = "User Is not Signed In"
-		response.message = "User has not signed in."
-		response.result = False
+		parameters = await extract_all_parameters(request)
 
-		json_response = JSONResponse(
-			status_code=APIResponseStatus.EXPECTATION_FAILED_ERROR.http_code,
-			content=response.toDict()
-		)
+		token = extract_jwt_token(parameters)
 
-	return json_response
+		user = get_user(token)
+
+		if user or not validate_request_token(token):
+			response.title = "User is Signed In"
+			response.message = "User has already signed in."
+			response.result = True
+
+			return JSONResponse(
+				status_code=APIResponseStatus.SUCCESS.http_code,
+				content=response.toDict()
+			)
+		else:
+			response.title = "User Is not Signed In"
+			response.message = "User has not signed in."
+			response.result = False
+
+			return JSONResponse(
+				status_code=APIResponseStatus.EXPECTATION_FAILED_ERROR.http_code,
+				content=response.toDict()
+			)
+	except HTTPException as exception:
+		if exception.status_code == APIResponseStatus.UNAUTHORIZED_ERROR.http_code:
+			response.title = "User Is Not Authorized"
+			response.message = "User token has expired."
+			response.result = False
+
+			return JSONResponse(
+				status_code=APIResponseStatus.EXPECTATION_FAILED_ERROR.http_code,
+				content=response.toDict()
+			)
+
+		raise exception
 
 
 @app.get("/service/status")
