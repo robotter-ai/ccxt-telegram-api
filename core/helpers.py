ee151e573e74744f4914ff89654cbc6119e18b00
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-09-26 21:03:47 -0300

    Simplifying helpers.py

--- core/helpers.py
+++ core/helpers.py
@@ -17,10 +17,9 @@ import ccxt.async_support as async_ccxt
 from ccxt import Exchange as RESTExchange
 from ccxt.async_support import Exchange as WebSocketExchange
 from core.constants import constants
-from core.cypher import cypher
-from core.database import database
 from core.properties import properties
 from core.types import Protocol, Credentials, Environment, WebSocketCloseCode
+from core.users import users
 from core.utils import deep_merge
 
 ccxt = sync_ccxt
@@ -101,106 +100,12 @@ def extract_jwt_token(parameters: DotMap[str, Any]):
 
 
 def get_user_exchange(id_or_user_telegram_id_or_jwt_token: str | int, exchange_id: str, exchange_environment: Environment, exchange_protocol: Protocol) -> Optional[RESTExchange | WebSocketExchange]:
-	user = get_user(id_or_user_telegram_id_or_jwt_token)
+	user = users.get(id_or_user_telegram_id_or_jwt_token)
 
 	if user:
 		return properties.get_or_default(f"""users.{user.id}.exchange.{exchange_id}.{exchange_environment.value}.{exchange_protocol.value}""")
 
 	return None
-
-
-def get_user(id_or_user_telegram_id_or_jwt_token: str | int) -> Optional[DotMap[str, Any]]:
-	user = properties.get_or_default(f"""users.{id}""", None)
-
-	if not user:
-		user_id = properties.get_or_default(f"""telegram.ids.{id_or_user_telegram_id_or_jwt_token}""")
-		user = properties.get_or_default(f"""users.{user_id}""", None)
-
-	if not user:
-		user_id = properties.get_or_default(f"""tokens.{id_or_user_telegram_id_or_jwt_token}""")
-		user = properties.get_or_default(f"""users.{user_id}""", None)
-
-	if user:
-		return DotMap(user, _dynamic=False)
-
-	return None
-
-
-def update_user(credentials: Credentials) -> DotMap[str, Any]:
-	user_exists = database.select_single_value("""SELECT EXISTS(SELECT 1 FROM user WHERE id = :id);""", {"id": credentials.id})
-
-	if not user_exists:
-		database.insert(
-			"""
-				INSERT INTO
-					user
-						(id, exchange_id, exchange_environment, telegram_id, api_key, api_secret, sub_account_id, data)
-					VALUES
-						(:id, :exchange_id, :exchange_environment, :telegram_id, :api_key, :api_secret, :sub_account_id, :data)
-			""", {
-				"id": credentials.id,
-				"exchange_id": credentials.exchangeId,
-				"exchange_environment": credentials.exchangeEnvironment,
-				"telegram_id": cypher.encrypt(credentials.userTelegramId),
-				"api_key": cypher.encrypt(credentials.exchangeApiKey),
-				"api_secret": cypher.encrypt(credentials.exchangeApiSecret),
-				"sub_account_id": cypher.encrypt(credentials.exchangeOptions.get("subAccountId") if credentials.exchangeOptions else ""),
-				"data": str({
-					"favorites": {
-						"tokens": [],
-						"markets": []
-					}
-				}),
-			},
-			True
-		)
-		user = database.select_single("select * from user where id = :id", {"id": credentials.id})
-
-		print(user)
-
-	rest_exchange: RESTExchange = getattr(ccxt, credentials.exchangeId)({
-		"apiKey": credentials.exchangeApiKey,
-		"secret": credentials.exchangeApiSecret,
-		"options": {
-			"environment": credentials.exchangeEnvironment,
-			"subaccountId": credentials.exchangeOptions.get("subAccountId"),
-		}
-	})
-
-	websocket_exchange: WebSocketExchange = getattr(async_ccxt, credentials.exchangeId)({
-		"apiKey": credentials.exchangeApiKey,
-		"secret": credentials.exchangeApiSecret,
-		"options": {
-			"environment": credentials.exchangeEnvironment,
-			"subaccountId": credentials.exchangeOptions.get("subaccountid"),
-		}
-	})
-
-	if credentials.exchangeEnvironment != constants.environments.production:
-		rest_exchange.set_sandbox_mode(True)
-		websocket_exchange.set_sandbox_mode(True)
-
-	rest_exchange.fetch_markets()
-	rest_exchange.fetch_currencies()
-
-	properties.set(f"""users.{credentials.id}.id""", credentials.id)
-	properties.set(f"""users.{credentials.id}.exchange.{credentials.exchangeId}.{credentials.exchangeEnvironment}.credentials""", credentials)
-	properties.set(f"""users.{credentials.id}.exchange.{credentials.exchangeId}.{credentials.exchangeEnvironment}.{Protocol.REST.value}""", rest_exchange)
-	properties.set(f"""users.{credentials.id}.exchange.{credentials.exchangeId}.{credentials.exchangeEnvironment}.{Protocol.WebSocket.value}""", websocket_exchange)
-
-	properties.set(f"""telegram.ids.{credentials.userTelegramId}""", credentials.id)
-	properties.set(f"""tokens.{credentials.jwtToken}""", credentials.id)
-
-	return properties.get_or_default(f"""users.{credentials.id}""")
-
-
-def delete_user(idOrJwtToken: str):
-	user = get_user(idOrJwtToken)
-
-	if user:
-		properties.set(f"""users.{user.id}""", None)
-	# properties.set(f"""telegram.ids.{userTelegramId}""", None)
-	# properties.set(f"""tokens.{jwtToken}""", None)
 
 
 def create_jwt_token(data: dict, expires_delta: datetime.timedelta):
